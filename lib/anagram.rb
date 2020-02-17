9934ade9b3202750040c380b79fb53e257542ff2
Hailey Bongo <haileyebongo@gmail.com>
2020-02-17 22:45:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+
+ class Anagram
+    attr_accessor :word
+    
+   def initialize(word)
+     @word = word 
+   end
+   
+   def match(array)
+     match = []
+     new_word = @word.split("")
+     array.each do |word|
+       letters = word.split("")
+       if (new_word.size == letters.size) && (new_word.sort == letters.sort) 
+         match << letters.join
+      else
+      end
+    end
+    match
+  end
+
+
+end
