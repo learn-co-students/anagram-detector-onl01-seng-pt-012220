4c309dd38b92f093094bfef114918a782f9ecdf9
Xavier Perez <xperez007@gmail.com>
2020-02-18 16:38:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :name,:anagram
+  @@all = []
+  .match = match
+end
+
+end
+
+def initialize(one, two, three)
+  @array = array
+  listen = Anagram.new("listen")
+  listen.match(%w(one, two, three)
+end
+
+end
+
+def (one, three, two).sort == (one, two, three) .sort 
+=> False 
+
+elsif (one, two, three) == (three, two, one) .sort
+=> True 
+
+end
+
+def self.all 
+  @@all
+end
+
+end
+
+
